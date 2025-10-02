18a11bbc8ae263256bdd7717c5e8c873ac17b2a9
Sunny Chan <schan6803@gmail.com>
2025-10-01 22:26:15 -0400

    repo init

---
+++ client/src/App.tsx
@@ -0,0 +1,19 @@
+import { useState, useEffect } from 'react'
+import './App.css'
+
+function App() {
+  const [ message, setMessage ] = useState('');
+  useEffect(() => {
+    fetch('/')
+      .then((res) => res.text())
+      .then((data) => setMessage(data))
+      .catch((err) => console.log(err));
+  }, []);
+  return (
+    <div>
+      <h1> {message} </h1>
+    </div>
+  );
+}
+
+export default App;
