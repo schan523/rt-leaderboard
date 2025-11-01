fd79f0c59a8dd6dddf2a91807587dec7dca00a71
Sunny Chan <schan6803@gmail.com>
2025-11-01 02:12:22 -0400

    .

--- client/src/components/layouts/Navbar.tsx
+++ client/src/components/layouts/Navbar.tsx
@@ -1,10 +1,16 @@
 import { Link } from 'react-router-dom';
+import '../../styles/Navbar.css';
 
 export const Navbar = () => {
     return (
         <div className="navbar-group">
-            <Link to="/"> Home </Link>
-            <Link to="../register"> Register </Link>
+            <div className="link-border">
+                <Link to="/"> Home </Link>
+            </div>
+            <div className="link-border">
+                <Link to="../register"> Register </Link>
+            </div>
+            
         </div>
     );
 }
