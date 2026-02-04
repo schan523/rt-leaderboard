3d8a739ef4a9f0592681f248faa037c3df646b7a
Sunny Chan <schan6803@gmail.com>
2026-02-04 04:01:40 -0500

    added placeholder span to navbar indicating when a user is logged in

--- client/src/components/layouts/Navbar.tsx
+++ client/src/components/layouts/Navbar.tsx
@@ -1,7 +1,25 @@
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { authContextValue } from '../../context/AuthContext';
 import '../../styles/Navbar.css';
 
+import * as jose from 'jose'; 
+
 export const Navbar = () => {
+    const { token } = authContextValue();
+    const [username, setUsername] = useState("");
+
+    useEffect(() => {
+        const fetchUsername = async () => {
+            const secret = new TextEncoder().encode(import.meta.env.VITE_TOKEN_SECRET);
+           const { payload } = await jose.jwtVerify(token, secret);
+           setUsername(payload.username);
+            console.log(username);
+        }
+        fetchUsername();
+    }, [token]);
+
+    console.log("this should be the username:", username);
     return (
         <div className="navbar-group">
             <div className="link-border">
@@ -16,6 +34,11 @@ export const Navbar = () => {
             <div className="link-border">
                 <Link to="../score"> Score </Link>
             </div>
+            { token &&
+                <div className="link-border">
+                    <span> { username } </span>
+                </div> 
+            }
         </div>
     );
 }
