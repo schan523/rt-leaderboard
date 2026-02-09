6fde3ae325aa6a195486422e32fc557706a14d86
Sunny Chan <schan6803@gmail.com>
2026-02-08 20:32:13 -0500

    working on score submission component

--- client/src/components/Score.tsx
+++ client/src/components/Score.tsx
@@ -1,12 +1,23 @@
+import { authContextValue } from '../context/authContext.tsx';
+import { useForm } from 'react-hook-form';
+
+type scoreFormData = {
+
+}
+
 export const Score = () => {
-    async function submit(formData: FormData) {
-        const data = {};
-        for (const [name, value] of formData) {
-            data[name] = value;
-        }
+    const { token, setToken } = authContextValue();
+    const {
+        register,
+        handleSubmit,
+        formState: { errors }
+    } = useForm<scoreFormData>();
+
+    const submit = async (formData: scoreFormData) => {
+        const data : Record<string, string> = {}
 
         // Need to store authorization JWT to frontend 
-        await fetch('/api/lb/submit', {
+        const response = await fetch('/api/lb/submit', {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
@@ -17,24 +28,39 @@ export const Score = () => {
     }
 
     return (
-        <form action={ submit }>
-            <label htmlFor="game">Select a game</label>
-            <select name="game" id="game">
-                <option value="hollow-knight"> Hollow Knight </option>
-                <option value="silksong"> Silksong </option>
-                <option value="minecraft"> Minecraft </option>
-            </select>
-            <br />
-            <span>
-                Time:
-                <input type="text" id="hours"></input>
-                :
-                <input type="text" id="minutes"></input>
-                :
-                <input type="text" id="seconds"></input>
-            </span>
-            <br />
-            <button type="submit"> Submit </button>
-        </form>
+        <div>
+            <form onSubmit={handleSubmit(submit)}>
+                <label htmlFor="game">Select a game </label>
+                <select {...register("game")}>
+                    <option value="hollow-knight"> Hollow Knight </option>
+                    <option value="silksong"> Silksong </option>
+                    <option value="minecraft"> Minecraft </option>
+                </select>
+                <br />
+                <button type="submit"> Submit </button>
+            </form>
+        </div>
     );
+
+    // return (
+    //     <form action={ submit }>
+    //         <label htmlFor="game">Select a game</label>
+    //         <select name="game" id="game">
+    //             <option value="hollow-knight"> Hollow Knight </option>
+    //             <option value="silksong"> Silksong </option>
+    //             <option value="minecraft"> Minecraft </option>
+    //         </select>
+    //         <br />
+    //         <span>
+    //             Time:
+    //             <input type="text" id="hours"></input>
+    //             :
+    //             <input type="text" id="minutes"></input>
+    //             :
+    //             <input type="text" id="seconds"></input>
+    //         </span>
+    //         <br />
+    //         <button type="submit"> Submit </button>
+    //     </form>
+    // );
 }
