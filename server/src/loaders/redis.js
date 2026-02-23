7d522f1a6613da80f655be3ddbaed62ee83b7a39
Sunny Chan <schan6803@gmail.com>
2026-02-22 19:56:11 -0500

    started redis operations

--- server/src/loaders/redis.js
+++ server/src/loaders/redis.js
@@ -16,4 +16,6 @@ client.on('error', err => console.log('Redis Client Error', err));
 
 await client.connect();
 
+export { client }
+
 
--- server/src/services/leaderboard.ts
+++ server/src/services/leaderboard.ts
@@ -1,6 +1,7 @@
 import 'dotenv/config';
 import db from '../loaders/mongoose.js';
 import { scoreModel } from '../models/score.js';
+import { client } from '../loaders/redis.js';
 
 export default class leaderboardService {
     static async submit(username: string, game:string, hours: string, minutes: string, seconds: string) {
@@ -9,5 +10,6 @@ export default class leaderboardService {
         const score = new scoreModel({username: username, game: game, hours: parseInt(hours), minutes: parseInt(minutes), seconds: parseInt(seconds)});
         await score.save();
 
+        const res1 = await client.zAdd();
     }
 }
