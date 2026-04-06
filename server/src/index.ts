4802b49e31729733aff661a9739bcccfedac9788
Sunny Chan <schan6803@gmail.com>
2026-04-06 14:31:27 -0400

    removing session middleware

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,5 @@
 import express, { Request, Response } from 'express';
 import cookieParser from 'cookie-parser';
-import session from 'express-session';
 
 import { userRouter } from './routes/user';
 import { lbRouter } from './routes/leaderboard';
@@ -10,17 +9,6 @@ const port = 3000;
 
 app.set('trust proxy', 1);
 app.use(cookieParser());
-
-// sameSite: 'strict'
-app.use(session({
-    secret: process.env.SESSION_SECRET,
-    resave: false,
-    saveUninitialized: false,
-    cookie: {
-        httpOnly: true, 
-        maxAge: 86400000
-    }
-}));
 
 app.use(express.json());
 app.use('/', userRouter);
