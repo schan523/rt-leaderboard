ecab285c2b62b79ed5b70f4374c1558093164300
Sunny Chan <schan6803@gmail.com>
2026-04-06 22:36:26 -0400

    cleaning

--- client/src/components/Leaderboard.tsx
+++ client/src/components/Leaderboard.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router';
+// import { useNavigate } from 'react-router';
 import { authContextValue } from '../context/authContext.tsx';
 import { customFetch } from '../helpers/index.ts';
 
@@ -9,7 +9,7 @@ import { Board } from './Board.tsx';
 export const Leaderboard = () => {
     const { token } = authContextValue();
     const [boards, setBoards] = useState<any[]>([]);
-    const navigate = useNavigate();
+    // const navigate = useNavigate();
 
     // useEffect(() => {
     //     if (token == "") {
